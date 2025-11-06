b45e97603cf730feac16d3d8c2e8a4189e64f236
PriyaPandey19 <pp479021@gmail.com>
2025-11-06 19:09:23 +0530

    changes

--- controllers/users.js
+++ controllers/users.js
@@ -5,16 +5,15 @@ module.exports.renderSignupForm = (req,res)=>{
 }
 
 
-module.exports.signup = async(req,res) =>{
+module.exports.signup = async(req,res,next) =>{
     try{
     let{username, email,password} = req.body;
     const newUser = new User({email, username});
     const registeredUser = await User.register(newUser, password);
     console.log(registeredUser);
     req.login(registeredUser,(err) =>{
-        if(err){
+        if(err)
             return next(err);
-        }
       req.flash("success","Welcome to Wanderlust!");
     res.redirect("/listings");  
     })
