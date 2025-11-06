0b8563340ae5812a471facb4a4319774d35a6b4d
PriyaPandey19 <pp479021@gmail.com>
2025-11-06 19:14:53 +0530

    changes

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ const sessionOptions = {
     saveUninitialized:true,
     cookie:{
           httpOnly: true,
-          secure:true,
+          secure:false,
           sameSite: "None",
-        expires:Date.now() +7 * 24 * 60 *60 *1000,
+        expires:new Date(Date.now() +7 * 24 * 60 *60 *1000),
         maxAge: 7 * 24 * 60 * 60 * 1000
     },
 };
