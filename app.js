101aba9f61fe291b12976d6a295d22d1928a0371
PriyaPandey19 <pp479021@gmail.com>
2025-11-06 18:30:07 +0530

    changes

--- app.js
+++ app.js
@@ -122,6 +122,9 @@ app.use((req,res,next) =>{
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 
 
