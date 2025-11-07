14a212675b93cc380dee3c6628f7555ce9600bb2
PriyaPandey19 <pp479021@gmail.com>
2025-11-07 23:43:20 +0530

    changes

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,6 +1,5 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
 const uploadFolder = process.env.NODE_ENV === 'development' ? 'wanderlust_DEV' : 'wanderlust_PROD';
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
